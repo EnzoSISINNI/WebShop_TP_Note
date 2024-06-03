664801e45f6ea486fa4917b851ff8c05ae46df2e
EnzoSISINNI <savilsisinni@gmail.com>
2024-06-03 21:56:00 +0200

    Rendu Projet

---
+++ src/components/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Webshop</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+};
+</script>
+
+<style scoped>
+/* Ajoutez des styles personnalisés ici si nécessaire */
+</style>
+
+  
